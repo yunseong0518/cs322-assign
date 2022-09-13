27d88fe605d35f8c791f21a08a7d4fdba6ab37a4
yunseong0518 <yunseong0518@postech.ac.kr>
2022-09-13 22:19:18 +0900

    complete 1-1 pascal

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,7 +14,13 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else if (c == r) 1
+    else {
+      pascal(c - 1, r -1) + pascal(c, r - 1)
+    }
+  } 
 
   /**
    * Exercise 2
