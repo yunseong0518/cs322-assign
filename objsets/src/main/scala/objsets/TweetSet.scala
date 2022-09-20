c48f2e3c350cd9143a00e6016cfc4bd25240dcda
yunseong0518 <yunseong0518@postech.ac.kr>
2022-09-20 22:40:13 +0900

    complete 3-2 taking unions

--- objsets/src/main/scala/objsets/TweetSet.scala
+++ objsets/src/main/scala/objsets/TweetSet.scala
@@ -55,7 +55,7 @@ abstract class TweetSet {
    * Question: Should we implment this method here, or should it remain abstract
    * and be implemented in the subclasses?
    */
-   def union(that: TweetSet): TweetSet = ???
+   def union(that: TweetSet): TweetSet = filterAcc(_ => true, that)
 
   /**
    * Returns the tweet from this set which has the greatest retweet count.
