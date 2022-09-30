c015d1e0eee44e2c328d14c0552e1d58cf4840ee
yunseong0518 <yunseong0518@postech.ac.kr>
2022-09-30 23:36:54 +0900

    complete 4-4b-1 encoding using code table

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -227,7 +227,9 @@ object Huffman {
    * This function returns the bit sequence that represents the character `char` in
    * the code table `table`.
    */
-  def codeBits(table: CodeTable)(char: Char): List[Bit] = ???
+  def codeBits(table: CodeTable)(char: Char): List[Bit] = {
+    table.filter(x => x._1 == char).head._2
+  }
 
   /**
    * Given a code tree, create a code table which contains, for every character in the
