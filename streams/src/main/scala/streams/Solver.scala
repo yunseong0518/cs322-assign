d566ea1a8f01a80f2e221984cd1115a3dd7052f7
yunseong0518 <yunseong0518@postech.ac.kr>
2022-11-03 14:57:45 +0900

    complete 6-6 avoiding circles

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -38,7 +38,9 @@ trait Solver extends GameDef {
    * make sure that we don't explore circular paths.
    */
   def newNeighborsOnly(neighbors: Stream[(Block, List[Move])],
-                       explored: Set[Block]): Stream[(Block, List[Move])] = ???
+                       explored: Set[Block]): Stream[(Block, List[Move])] = {
+                        neighbors.filter(neighbor => !explored.exists(_ == neighbor._1))
+                       }
 
   /**
    * The function `from` returns the stream of all possible paths
